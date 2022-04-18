1c9b6fe7ba707818e7762c4657b541ea0530fd37
Thijn <Ralkey@outlook.com>
2022-04-18 11:58:01 +0200

    Update unlockHDScreenShare.plugin.js
    premium type now goes back to original when plugin disabled

--- unlockHDScreenShare.plugin.js
+++ unlockHDScreenShare.plugin.js
@@ -1,19 +1,24 @@
 /**
  * @name unlockHDScreenShare
- * @version 1.0.0
+ * @version 0.0.1
  * @description unlock the ability to share your screen in 1080p
  * @author Ralkey#0516
+ * @source https://github.com/RalkeyOfficial/discord-1080p-video-unlocker
  *   
  */
 
 
 module.exports = class unlockHDScreenShare{
      
+    load() {
+        this.userPremiumType = BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType;
+    }
+     
     start() { 
         BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType = 2;
     }
     
     stop() {
-
+        BdApi.findModuleByProps("getCurrentUser").getCurrentUser().premiumType = this.userPremiumType;
     }
 }
